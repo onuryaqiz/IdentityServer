b747226754f15247a9252939ff31926a91c3d0fc
Onur Yağız <95749456+onuryaqiz@users.noreply.github.com>
2022-11-24 12:31:37 +0300

    Add project files.

---
+++ IdentityServer/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityServer.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
